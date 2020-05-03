fc2c2e6ec711ed89e81fcdcfd378f6800d21fef2
ahmed mohamed <ahmedmohamed26.github.io>
2020-05-03 18:30:44 +0200

    add spinner in team details

--- src/pages/team/team.jsx
+++ src/pages/team/team.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect ,Fragment} from 'react';
 import './team.scss';
 import {
 	TeamDetails,
@@ -7,10 +7,14 @@ import {
 } from './../../services/competitions';
 import { Col, Container, Row } from 'reactstrap';
 import defaultTeamLogo from '../../assets/images/defaultTeamLogo.png';
+import Loading from '../../components/loading/loading';
+
+
 const Team = (props) => {
 	const [team, setTeam] = useState();
 	const [nextMatch, setNextMatch] = useState();
 	const [lastMatch, setLastMatch] = useState();
+	const [loadSpinner, setloadSpinner] = useState(false);
 	let ID = props.match.params.id;
 	useEffect(() => {
 		getTeamDetails();
@@ -19,23 +23,35 @@ const Team = (props) => {
 	}, []);
 
 	const getTeamDetails = () => {
+		setloadSpinner(true)
 		TeamDetails(ID)
-			.then(({ data }) => setTeam(data.teams[0]))
+			.then(({ data }) => {
+				setTeam(data.teams[0])
+				setloadSpinner(false)
+			})
 			.catch((error) => {
 				throw new Error(error.message);
 			});
 	};
 	function getNextMatch() {
+		setloadSpinner(true)
 		NextMatch(ID)
-			.then(({ data }) => setNextMatch(data.events[0]))
+			.then(({ data }) => {
+				setNextMatch(data.events[0])
+				setloadSpinner(false)
+			})
 			.catch((error) => {
 				throw new Error(error.message);
 			});
 	}
 
 	function getLastMatch() {
+		setloadSpinner(true)
 		LastMatch(ID)
-			.then(({ data }) => setLastMatch(data.results[0]))
+			.then(({ data }) => {
+				setLastMatch(data.results[0])
+				setloadSpinner(false)
+			})
 			.catch((error) => {
 				throw new Error(error.message);
 			});
@@ -46,22 +62,25 @@ const Team = (props) => {
 			<Container>
 				<Row>
 					<Col>
+					
 						<div className='parent mt-0'>
-							<div className='title d-flex align-items-center'>
+						{!loadSpinner  ? (<div className='title d-flex align-items-center'>
 								<img src={team?.strTeamBadge} alt={team?.strTeam} />
 								<div className='ml-3'>
 									<h6 className='mb-0'> {team?.strCountry} </h6>
 									<h2 className='mb-0'> {team?.strTeam} </h2>
-									<h5 className='mb-0'> {team?.strLeague} </h5>
+									<h5 className='mb-0'> {team?.strLeague}</h5>
 								</div>
-							</div>
+							</div>) : (<Loading />)} 
+							
 						</div>
 					</Col>
 				</Row>
 				<Row>
 					<Col md='5'>
 						<div className='parent'>
-							<div className='details-match'>
+							{!loadSpinner  ? (<Fragment>
+								<div className='details-match'>
 								<h2> next match </h2>
 								<div className='date-match'>
 									<h4 className='text-center'>
@@ -89,37 +108,39 @@ const Team = (props) => {
 								</div>
 							</div>
 							<div className='details-match'>
-								<h2> last match </h2>
-								<div className='date-match'>
-									<h4 className='text-center'>
-										{`${new Date(lastMatch?.dateEvent).getMonth() +
-											1} / ${new Date(lastMatch?.dateEvent).getDate()}`}
-									</h4>
-									<div className='d-flex justify-content-around align-items-center'>
-										<div className='away-team text-center'>
-											<img src={team?.strTeamBadge} alt={team?.strTeam} />
-											<h6> {team?.strTeam} </h6>
-										</div>
-										<div>
-											<h3 className='font-weight-bold'> VS </h3>
-										</div>
-										<div className='home-team text-center'>
-											<img src={defaultTeamLogo} alt={'asda'} />
-											<h6>
-												{team?.strTeam !== lastMatch?.strAwayTeam
-													? lastMatch?.strAwayTeam
-													: lastMatch?.strHomeTeam}
-											</h6>
-										</div>
+							<h2> last match </h2>
+							<div className='date-match'>
+								<h4 className='text-center'>
+									{`${new Date(lastMatch?.dateEvent).getMonth() +
+										1} / ${new Date(lastMatch?.dateEvent).getDate()}`}
+								</h4>
+								<div className='d-flex justify-content-around align-items-center'>
+									<div className='away-team text-center'>
+										<img src={team?.strTeamBadge} alt={team?.strTeam} />
+										<h6> {team?.strTeam} </h6>
 									</div>
-									<h5 className='text-center'> {lastMatch?.strFilename} </h5>
+									<div>
+										<h3 className='font-weight-bold'> VS </h3>
+									</div>
+									<div className='home-team text-center'>
+										<img src={defaultTeamLogo} alt={'asda'} />
+										<h6>
+											{team?.strTeam !== lastMatch?.strAwayTeam
+												? lastMatch?.strAwayTeam
+												: lastMatch?.strHomeTeam}
+										</h6>
+									</div>
 								</div>
+								<h5 className='text-center'> {lastMatch?.strFilename} </h5>
 							</div>
+						</div> 
+						</Fragment> ) : (<Loading />)}
+							
 						</div>
 					</Col>
 					<Col>
 						<div className='parent'>
-							<div className='details-team'>
+							{!loadSpinner  ? (<div className='details-team'>
 								<img src={team?.strStadiumThumb} alt={team?.strStadiumThumb} />
 								<h1>{team?.strAlternate}</h1>
 								<p>{team?.strDescriptionEN}</p>
@@ -165,7 +186,8 @@ const Team = (props) => {
 										</a>
 									</li>
 								</ul>
-							</div>
+							</div>) : (<Loading />)}
+							
 						</div>
 					</Col>
 				</Row>
